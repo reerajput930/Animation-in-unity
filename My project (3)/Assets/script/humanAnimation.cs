12c0f7476a3888db451038901952cb707ad6d411
Riya <rajputriya930@gmail.com>
2022-03-06 18:05:23 +0530

    more movements

--- My project (3)/Assets/script/humanAnimation.cs
+++ My project (3)/Assets/script/humanAnimation.cs
@@ -9,7 +9,7 @@ public class humanAnimation : MonoBehaviour
     void Start()
     {
         Debug.Log("hello world");
-            //animator.SetBool("isWalking",false);       
+        //animator.SetBool("isWalking",false);       
     }
 
     // Update is called once per frame
@@ -19,12 +19,16 @@ public class humanAnimation : MonoBehaviour
         bool fastWalking = animator.GetBool("fastWalking");
         bool jump = animator.GetBool("jump");
         bool dance = animator.GetBool("dance");
+        bool leftView = animator.GetBool("leftView");
+        bool rightView = animator.GetBool("rightView");
 
         bool forwardPressed = Input.GetKey("w");
         bool fastPressed = Input.GetKey("left shift");
         bool jumpPressed = Input.GetKey("space");
         bool dancePressed = Input.GetKey("q");
-         
+        bool leftViewPressed = Input.GetKey("a");
+        bool rightViewPressed = Input.GetKey("d");
+
         //is walking
         if (!isWalking && forwardPressed)
         {
@@ -34,7 +38,7 @@ public class humanAnimation : MonoBehaviour
         {
             animator.SetBool("isWalking", false);
         }
-       
+
         //walk -- then fast move
         //that means , fast walking possible only when the body is walking
         if ((forwardPressed) && (!fastWalking && fastPressed))
@@ -44,7 +48,7 @@ public class humanAnimation : MonoBehaviour
         if ((forwardPressed || !forwardPressed) && fastWalking && !fastPressed)
         {
             animator.SetBool("fastWalking", false);
-            
+
         }
 
         //making things jump
@@ -52,17 +56,35 @@ public class humanAnimation : MonoBehaviour
         {
             animator.SetBool("jump", true);
         }
-        if((isWalking && !forwardPressed )|| (jump && !jumpPressed))
+        if ((isWalking && !forwardPressed) || (jump && !jumpPressed))
         {
             animator.SetBool("jump", false);
         }
-        if( !dance && dancePressed)
+        if (!dance && dancePressed)
         {
             animator.SetBool("dance", true);
         }
-        if(dance && !dancePressed)
+        if (dance && !dancePressed)
         {
             animator.SetBool("dance", false);
         }
+        if ((isWalking && forwardPressed) && (!leftView && leftViewPressed))
+        {
+            animator.SetBool("leftView", true);
+        }
+        if ((forwardPressed || !forwardPressed) && leftView && !leftViewPressed)
+        {
+            animator.SetBool("leftView", false);
+        }
+
+        if ((isWalking && forwardPressed) && (!rightView && rightViewPressed))
+        {
+            animator.SetBool("rightView", true);
+        }
+        if ((forwardPressed || !forwardPressed) && rightView && !rightViewPressed)
+        {
+            animator.SetBool("rightView", false);
+        }
+
     }
 }
